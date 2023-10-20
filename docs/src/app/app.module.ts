ad2763e48f1e021eb3a40b8f34b4694fa7160546
Khushi Joshi <khushi.joshi@simptel.in>
2023-10-20 13:19:25 +0530

    Resolved imports

--- docs/src/app/app.module.ts
+++ docs/src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, SecurityContext } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GetStartedComponent } from './get-started/get-started.component';
 import { InputRepoComponent } from './input-repo/input-repo.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,14 @@ import { InputRepoComponent } from './input-repo/input-repo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({
+      loader: HttpClient,
+      sanitize: SecurityContext.NONE,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
