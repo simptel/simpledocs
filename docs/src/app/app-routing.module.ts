866c22690a4be384592e8717d54a776ad66051cc
Khushi Joshi <khushi.joshi@simptel.in>
2023-10-20 11:45:50 +0530

    app module

--- docs/src/app/app-routing.module.ts
+++ docs/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GetStartedComponent } from './get-started/get-started.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'getting-started',
+    component: GetStartedComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'getting-started',
+  },
+  {
+    path: '**',
+    redirectTo: 'getting-started',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- docs/src/app/app.module.ts
+++ docs/src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, SecurityContext } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InputRepoComponent } from './input-repo/input-repo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetStartedComponent,
+    SidemenuComponent,
+    InputRepoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({
+      loader: HttpClient,
+      sanitize: SecurityContext.NONE,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
