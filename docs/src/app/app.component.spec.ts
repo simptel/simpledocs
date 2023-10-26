005622411a16dda62381454d80fce42865a37f3d
Khushi Joshi <khushi.joshi@simptel.in>
2023-10-26 13:21:24 +0530

    app testing

--- docs/src/app/app.component.spec.ts
+++ docs/src/app/app.component.spec.ts
@@ -2,11 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, HttpClientModule],
+    declarations: [AppComponent, SidemenuComponent]
   }));
 
   it('should create the app', () => {
