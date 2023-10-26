9b14faf27703760398567859f3d64997d7192729
Khushi Joshi <khushi.joshi@simptel.in>
2023-10-26 10:33:36 +0530

    sidemenu

--- docs/src/app/app.module.ts
+++ docs/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidemenuComponent
   ],
   imports: [
     BrowserModule,
