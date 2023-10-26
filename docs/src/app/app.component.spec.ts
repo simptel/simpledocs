3f70388200b075d4b65ad0a80d121a5b4b1e0e10
Khushi Joshi <khushi.joshi@simptel.in>
2023-10-26 11:08:08 +0530

    app unit testing

--- docs/src/app/app.component.spec.ts
+++ docs/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -14,16 +15,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'docs'`, () => {
+  it(`should have as title 'Simpledocs'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('docs');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('docs app is running!');
+    expect(app.title).toEqual(environment.title);
   });
 });
