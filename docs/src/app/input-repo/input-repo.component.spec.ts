74523e64f8478a61f9813ed2147801707dabdc87
Khushi Joshi <khushi.joshi@simptel.in>
2023-10-26 13:21:12 +0530

    input-repo unit testing

--- docs/src/app/input-repo/input-repo.component.spec.ts
+++ docs/src/app/input-repo/input-repo.component.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputRepoComponent } from './input-repo.component';
+import { GithubService } from '../shared/github.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('InputRepoComponent', () => {
   let component: InputRepoComponent;
   let fixture: ComponentFixture<InputRepoComponent>;
+  let service: GithubService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [InputRepoComponent]
+      declarations: [InputRepoComponent],
+      providers: [GithubService, HttpClientTestingModule],
+      imports: [HttpClientTestingModule, FormsModule]
     });
+    service = TestBed.inject(GithubService);
     fixture = TestBed.createComponent(InputRepoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +24,18 @@ describe('InputRepoComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should save repo url', () => {
+    let repoUrl = 'https://github.com/simptel/docs.simptel.com';
+    const saveRepoSpy = spyOn(service, "saveRepoUrl");
+    const docsRefreshSpy = spyOn(service, "setDocsRefresh");
+
+    spyOn(component, 'saveRepoUrl').and.callThrough();
+    component.saveRepoUrl();
+
+    repoUrl = repoUrl.substring(19, repoUrl.length);
+    expect(saveRepoSpy).toHaveBeenCalledWith(repoUrl);
+    expect(docsRefreshSpy).toHaveBeenCalledWith(true);
   });
 });
--- docs/src/app/input-repo/input-repo.component.ts
+++ docs/src/app/input-repo/input-repo.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { GithubService } from '../shared/github.service';
-import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-input-repo',
@@ -10,15 +9,12 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class InputRepoComponent {
   repoUrl!: string;
 
-  constructor(private githubService: GithubService, private router: Router, 
-    private route: ActivatedRoute) {
-      this.repoUrl = 'https://github.com/simptel/docs.simptel.com';
+  constructor(private githubService: GithubService) {
+    this.repoUrl = 'https://github.com/simptel/docs.simptel.com';
   }
 
   saveRepoUrl() {
-    if (this.repoUrl) {
-      this.repoUrl = this.repoUrl.substring(19, this.repoUrl.length);
-    }
+    this.repoUrl = this.repoUrl.substring(19, this.repoUrl.length);
     this.githubService.saveRepoUrl(this.repoUrl);
     this.githubService.setDocsRefresh(true);
   }
