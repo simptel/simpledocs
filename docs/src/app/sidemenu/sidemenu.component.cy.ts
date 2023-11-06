97e1a35e2c67f9d1b5f5155a7c50dd4d35a3644c
khushi-1305 <khushi.joshi@simptel.in>
2023-11-06 12:37:46 +0530

    update sidemenu cypress

--- docs/src/app/sidemenu/sidemenu.component.cy.ts
+++ docs/src/app/sidemenu/sidemenu.component.cy.ts
@@ -1,9 +1,13 @@
 describe('should test sidemenu', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200/#/getting-started');
-    cy.get('#openMenu').click({force: true});
-    cy.get('#toggleTheme').click({force: true});
+  });
+
+  it('should test side menu', () => {
+    cy.visit('http://localhost:4200/#/getting-started');
+    cy.viewport(610, 900);
     cy.get('#showSidemenu').click({force: true});
+    cy.get('#toggleTheme').click({force: true});
     cy.get('#closeSidemenu').click({force: true});
   });
 });
