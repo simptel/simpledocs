67647496c899d3f3157eaa6b751a5fd840a1bb84
khushi-1305 <khushi.joshi@simptel.in>
2023-11-07 12:48:21 +0530

    updated

--- docs/src/app/sidemenu/sidemenu.component.cy.ts
+++ docs/src/app/sidemenu/sidemenu.component.cy.ts
@@ -1,13 +1,14 @@
 describe('should test sidemenu', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:4200/#/getting-started');
-  });
-
   it('should test side menu', () => {
     cy.visit('http://localhost:4200/#/getting-started');
     cy.viewport(610, 900);
     cy.get('#showSidemenu').click({force: true});
     cy.get('#toggleTheme').click({force: true});
+    cy.get('input[name="repo-url"]').clear();
+    cy.get('input[name="repo-url"]').type('https://github.com/simptel/docs.simptel.com', {force: true});
+    cy.get('button').click({force: true});
+    cy.get('#toggleTheme').click({force: true});
+    cy.get('#openMenu').click({force: true});
     cy.get('#closeSidemenu').click({force: true});
   });
 });
